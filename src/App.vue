ddd64b5d9199d74e019eafabd1f5503485047792
Seng Song <logeeker@gmail.com>
2020-09-23 17:32:07 +1000

    done

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,98 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2 class="mb-5">2 Stroke Fuel Mixing Calculator</h2>
+    
+    <b-container fluid="lg" class="p-3">
+        <div class="d-flex flex-column align-items-start">
+          <b-input-group>
+            <template v-slot:prepend>
+              <b-input-group-text >Fuel</b-input-group-text>
+            </template>
+            <b-form-input v-model="volume" size="md" type="number"></b-form-input>
+
+            <template v-slot:append>
+              <b-dropdown :text="buttonText" variant="dark" class="text-light">
+                <b-dropdown-item @click="selectUnit(1000,'L')">L</b-dropdown-item>
+                <b-dropdown-item @click="selectUnit(1,'ml')">ml</b-dropdown-item>
+                <b-dropdown-item @click="selectUnit(3785.41,'gal')">gal</b-dropdown-item>
+              </b-dropdown>
+            </template>
+          </b-input-group>
+
+          <b-form-checkbox v-model="isPercentage" switch size="lg" class="my-3">
+            {{switchText}}
+          </b-form-checkbox>
+
+          <b-input-group v-if="isPercentage" size="md" append="%">
+            <b-form-input v-model="percent" type="number" placeholder="Enter Percentage"></b-form-input>
+          </b-input-group>
+
+          <b-input-group v-if="!isPercentage" size="md" append=": 1">
+            <b-form-input v-model="ratio" type="number" placeholder="Enter Ratio"></b-form-input>
+          </b-input-group>
+
+          <h3 class="mt-3" v-if="result>0">Add <b>{{result}} ml</b> of oil</h3>
+
+        </div>
+
+    </b-container>
+
+    <v-footer padless absolute>
+      <v-col class="text-center" cols="12">
+        <a href="terms.html" target="_blank" class="text-dark mx-4">Terms of Use</a>
+        <a href="https://github.com/logeeker/2strokemix" class="text-dark mx-4" target="_blank">Source</a>
+      </v-col>
+      <v-col class="text-center" cols="12">
+        © {{ new Date().getFullYear() }} <strong>Seng Song</strong> | All Rights Reserved
+      </v-col>
+    </v-footer>
+      
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: function(){
+    return {
+      buttonText:"L",
+      volume:'',
+      multiplier:1000,
+      isPercentage:true,
+      percent:'',
+      ratio:0,
+      dialog:false
+    }
+  },
+  computed:{
+    volumeInml:function(){
+      return this.volume*this.multiplier
+    },
+    switchText:function(){
+      if (this.isPercentage){
+        return "Using Percentage";
+      }else{
+        return "Using Ratio";
+      }
+    },
+    result:function(){
+      if (this.isPercentage){
+        
+        return Math.round((this.percent*this.volumeInml/100)*100)/100;
+      }else{
+        var p=1/this.ratio;
+        
+        return Math.round((this.volumeInml*p)*100)/100;
+      }
+    }
+  },
+  methods:{
+    selectUnit:function(m,text){
+      this.buttonText=text;
+      this.multiplier=m
+    }
   }
+  
 }
 </script>
 
